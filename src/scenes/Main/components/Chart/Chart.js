8fda69d8f7055c0ce7e20bb6d269e74a1d6c028d
Sergey Yuhimovich <sergey.yuhimovich@gmail.com>
2017-08-14 14:26:39 +0300

    Charts implementation improvement

--- src/scenes/Main/components/Chart/Chart.js
+++ src/scenes/Main/components/Chart/Chart.js
@@ -5,63 +5,63 @@ import './Chart.css';
 const Chart = ( {type, title} ) => {
 
     const temperatures = [
-        {month: 'Jan', 'Temperature': [1, 10]},
-        {month: 'Feb', 'Temperature': [2, 13]},
-        {month: 'Mar', 'Temperature': [4, 15]},
-        {month: 'Apr', 'Temperature': [8, 21]},
-        {month: 'May', 'Temperature': [10, 24]},
-        {month: 'Jun', 'Temperature': [15, 31]},
-        {month: 'Jul', 'Temperature': [17, 33]},
-        {month: 'Aug', 'Temperature': [16, 36]},
-        {month: 'Sep', 'Temperature': [14, 30]},
-        {month: 'Oct', 'Temperature': [12, 27]},
-        {month: 'Nov', 'Temperature': [8, 21]},
-        {month: 'Dec', 'Temperature': [3, 15]},
+        {month: 'Jan', t: [1, 10]},
+        {month: 'Feb', t: [2, 13]},
+        {month: 'Mar', t: [4, 15]},
+        {month: 'Apr', t: [8, 21]},
+        {month: 'May', t: [10, 24]},
+        {month: 'Jun', t: [15, 31]},
+        {month: 'Jul', t: [17, 33]},
+        {month: 'Aug', t: [16, 36]},
+        {month: 'Sep', t: [14, 30]},
+        {month: 'Oct', t: [12, 27]},
+        {month: 'Nov', t: [8, 21]},
+        {month: 'Dec', t: [3, 15]},
     ];
 
     const af = [
-        {month: 'Jan', 'Air frost days': 1},
-        {month: 'Feb', 'Air frost days': 2},
-        {month: 'Mar', 'Air frost days': 4},
-        {month: 'Apr', 'Air frost days': 8},
-        {month: 'May', 'Air frost days': 10},
-        {month: 'Jun', 'Air frost days': 15},
-        {month: 'Jul', 'Air frost days': 17},
-        {month: 'Aug', 'Air frost days': 16},
-        {month: 'Sep', 'Air frost days': 14},
-        {month: 'Oct', 'Air frost days': 12},
-        {month: 'Nov', 'Air frost days': 8},
-        {month: 'Dec', 'Air frost days': 3},
+        {month: 'Jan', afd: 1},
+        {month: 'Feb', afd: 2},
+        {month: 'Mar', afd: 4},
+        {month: 'Apr', afd: 8},
+        {month: 'May', afd: 10},
+        {month: 'Jun', afd: 15},
+        {month: 'Jul', afd: 17},
+        {month: 'Aug', afd: 16},
+        {month: 'Sep', afd: 14},
+        {month: 'Oct', afd: 12},
+        {month: 'Nov', afd: 8},
+        {month: 'Dec', afd: 3},
     ];
 
     const rain = [
-        {month: 'Jan', 'Rain mm': 1},
-        {month: 'Feb', 'Rain mm': 2},
-        {month: 'Mar', 'Rain mm': 4},
-        {month: 'Apr', 'Rain mm': 8},
-        {month: 'May', 'Rain mm': 10},
-        {month: 'Jun', 'Rain mm': 15},
-        {month: 'Jul', 'Rain mm': 17},
-        {month: 'Aug', 'Rain mm': 16},
-        {month: 'Sep', 'Rain mm': 14},
-        {month: 'Oct', 'Rain mm': 12},
-        {month: 'Nov', 'Rain mm': 8},
-        {month: 'Dec', 'Rain mm': 3},
+        {month: 'Jan', rmm: 1},
+        {month: 'Feb', rmm: 2},
+        {month: 'Mar', rmm: 4},
+        {month: 'Apr', rmm: 8},
+        {month: 'May', rmm: 10},
+        {month: 'Jun', rmm: 15},
+        {month: 'Jul', rmm: 17},
+        {month: 'Aug', rmm: 16},
+        {month: 'Sep', rmm: 14},
+        {month: 'Oct', rmm: 12},
+        {month: 'Nov', rmm: 8},
+        {month: 'Dec', rmm: 3},
     ];
 
     const sunHours = [
-        {month: 'Jan', 'Sun hours': 1},
-        {month: 'Feb', 'Sun hours': 2},
-        {month: 'Mar', 'Sun hours': 4},
-        {month: 'Apr', 'Sun hours': 8},
-        {month: 'May', 'Sun hours': 10},
-        {month: 'Jun', 'Sun hours': 15},
-        {month: 'Jul', 'Sun hours': 17},
-        {month: 'Aug', 'Sun hours': 16},
-        {month: 'Sep', 'Sun hours': 14},
-        {month: 'Oct', 'Sun hours': 12},
-        {month: 'Nov', 'Sun hours': 8},
-        {month: 'Dec', 'Sun hours': 3},
+        {month: 'Jan', sh: 1},
+        {month: 'Feb', sh: 2},
+        {month: 'Mar', sh: 4},
+        {month: 'Apr', sh: 8},
+        {month: 'May', sh: 10},
+        {month: 'Jun', sh: 15},
+        {month: 'Jul', sh: 17},
+        {month: 'Aug', sh: 16},
+        {month: 'Sep', sh: 14},
+        {month: 'Oct', sh: 12},
+        {month: 'Nov', sh: 8},
+        {month: 'Dec', sh: 3},
     ];
 
     return (
@@ -74,7 +74,7 @@ const Chart = ( {type, title} ) => {
                         <XAxis dataKey="month"/>
                         <YAxis/>
                         <Tooltip/>
-                        <Area type='monotone' dataKey='Temperature' stroke='#D32F2F' fillOpacity={0.5} fill="#f44336" />
+                        <Area type='monotone' dataKey='t' stroke='#D32F2F' name="Temperature range" unit=' °C' fillOpacity={0.5} fill="#f44336" />
                     </AreaChart>
                 </ResponsiveContainer>}
 
@@ -84,7 +84,7 @@ const Chart = ( {type, title} ) => {
                         <XAxis dataKey="month"/>
                         <YAxis/>
                         <Tooltip/>
-                        <Line dataKey='Air frost days' stroke='#2196F3' strokeWidth='3' />
+                        <Line dataKey='afd' name="Air frost" unit=" days" stroke='#2196F3' strokeWidth='3' />
                     </LineChart>
                 </ResponsiveContainer>}
 
@@ -93,7 +93,7 @@ const Chart = ( {type, title} ) => {
                         <XAxis dataKey="month"/>
                         <YAxis/>
                         <Tooltip label="hellow"/>
-                        <Bar dataKey='Rain mm' stroke='#64B5F6' fillOpacity={1} fill="#64B5F6" />
+                        <Bar dataKey='rmm' name="Rain" unit=" mm" stroke='#64B5F6' fillOpacity={1} fill="#64B5F6" />
                     </BarChart>
                 </ResponsiveContainer>}
 
@@ -102,7 +102,7 @@ const Chart = ( {type, title} ) => {
                         <XAxis dataKey="month"/>
                         <YAxis/>
                         <Tooltip/>
-                        <Area type='monotone' dataKey='Sun hours' stroke='#FBC02D' fillOpacity={0.5} fill="#FFEB3B" />
+                        <Area type='monotone' dataKey='sh' name="Sun" unit=" hours" stroke='#FBC02D' fillOpacity={0.5} fill="#FFEB3B" />
                     </AreaChart>
                 </ResponsiveContainer>}
             </div>
